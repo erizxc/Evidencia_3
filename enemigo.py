7a2166f922edf675c4e26963aa4abcc54db5db79
erizxc <erickcecilio57@gmail.com>
2026-04-16 15:49:36 -0600

    Se definieron los atributos para la clase enemigo

--- enemigo.py
+++ enemigo.py
@@ -1,5 +1,10 @@
 class Enemigo:
     def __init__(self, nombre, vida, daño, nivel, tipo):
-        pass
+        self.__nombre = nombre
+        self.__vida = vida
+        self.__daño = daño
+        self.__nivel = nivel
+        self.__tipo = tipo
     
+
 
